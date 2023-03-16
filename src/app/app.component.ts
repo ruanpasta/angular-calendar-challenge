d6e59523b7e2d3a11845ab552add7237603889cf
ruanpasta <ruan_augusto_pasta@hotmail.com>
2023-03-16 09:11:43 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calendar-challenge-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'calendar-challenge';
+}
